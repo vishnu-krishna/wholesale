108e54cec481dce1b22a809fa644cf6cf963d77e
Vishnu <vishnuchander.balakrishnan@anz.com>
2021-02-18 12:42:18 +1100

    Initial Push

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { Provider } from "react-redux";
+import configureStore from "./store";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import CounterComponent from "./components/counter";
+
+const store = configureStore();
+
+class App extends Component {
+  render() {
+    return (
+      <Provider store={store}>
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1 className="App-title">Welcome to CRA-REDUX-SAGA-TEMPLATE</h1>
+          </header>
+          <p className="App-intro">
+            Try the 1 second delayed counter below. All powererd by
+            <b> Create-React-App, Redux, Redux-Saga</b>
+          </p>
+          <p>
+            Try clicking the Button below with the Redux Chrome Extension
+            Installed
+          </p>
+          <CounterComponent />
+        </div>
+      </Provider>
+    );
+  }
 }
 
 export default App;

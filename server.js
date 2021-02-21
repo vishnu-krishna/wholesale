55168ea6193abfb0e7978baafbb91d0c9ab8d094
Vishnu <vishnuchander.balakrishnan@anz.com>
2021-02-21 16:02:09 +1100

    add cors

--- server.js
+++ server.js
@@ -2,9 +2,12 @@ const jsonServer = require('json-server')
 const app = jsonServer.create()
 const path = require('path')
 const express = require('express')
+const cors = require('cors')
 const middlewares = jsonServer.defaults()
 const router = jsonServer.router(path.join(__dirname, 'api', 'db.json'))
 const port = process.env.PORT || 3001
+
+app.use(cors())
 app.use('/db', middlewares, router)
 app.use(express.static(path.join(__dirname, 'build')))
 app.get('/*', function (req, res) {
